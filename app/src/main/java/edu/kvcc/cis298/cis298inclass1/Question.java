1b818129357cd700f1ce97c90692277e49c63226
Josh Weigel <jdweigel@gmail.com>
2016-09-14 16:39:21 -0400

    progress from first day of work on GeoQuiz.

---
+++ app/src/main/java/edu/kvcc/cis298/cis298inclass1/Question.java
@@ -0,0 +1,36 @@
+package edu.kvcc.cis298.cis298inclass1;
+
+/**
+ * Created by jweigel0843 on 9/14/2016.
+ */
+public class Question
+{
+	private int mTextResId;
+	private boolean mAnswerTrue;
+
+	public Question(int textResId, boolean answerTrue)
+	{
+		mTextResId = textResId;
+		mAnswerTrue = answerTrue;
+	}
+
+	public int getTextResId()
+	{
+		return mTextResId;
+	}
+
+	public void setTextResId(int textResId)
+	{
+		mTextResId = textResId;
+	}
+
+	public boolean isAnswerTrue()
+	{
+		return mAnswerTrue;
+	}
+
+	public void setAnswerTrue(boolean answerTrue)
+	{
+		mAnswerTrue = answerTrue;
+	}
+}
